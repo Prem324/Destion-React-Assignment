74951f90c25e091bcfd306df0c1c69286613bfe0
Prem324 <prem324r@gmail.com>
2025-03-06 18:21:49 +0530

    Make ProductManagementPortal component responsive

--- src/components/ProductManagementPortal/ProductItem/index.jsx
+++ src/components/ProductManagementPortal/ProductItem/index.jsx
@@ -5,11 +5,9 @@ import "./index.css";
 function ProductItem({ product }) {
   return (
     <div className="product-item">
-      <Link to={`product/${product.id}`}>
+      <Link className="product-details-link" to={`product/${product.id}`}>
         <img className="product-image" src={product.productImage} />
         <h2 className="product-name">{product.name}</h2>
-        <p className="product-price">Price: ${product.price}</p>
-        <p className="product-store">Store: {product.store}</p>
       </Link>
     </div>
   );
