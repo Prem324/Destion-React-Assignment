e26186b71e27b17a1fc2a315a0109768bf775e4d
Prem324 <prem324r@gmail.com>
2025-03-06 15:06:03 +0530

    Make the Home component responsive

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import homeImage from "../../assets/product-invoice-2.jpg";
+import homeImage from "../../assets/product-invoice-1.png";
 import "./index.css";
 
 const Home = () => {
@@ -8,6 +8,7 @@ const Home = () => {
       <div className="hero-section">
         <h1>Welcome to Our Application</h1>
         <p>Manage your invoices and products with ease.</p>
+        <button>Get Started</button>
       </div>
       <div>
         <img className="home-page-image" src={homeImage} alt="home-image" />
