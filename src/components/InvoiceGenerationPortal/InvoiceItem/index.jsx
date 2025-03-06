12c40f3dffd6281493281d5ca792d15225a837b5
Prem324 <prem324r@gmail.com>
2025-03-06 12:04:38 +0530

    Add componenets and Routes

---
+++ src/components/InvoiceGenerationPortal/InvoiceItem/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InvoiceItem = () => {
+  return (
+    <div>
+      <h1>Invoice Item</h1>
+    </div>
+  );
+};
+
+export default InvoiceItem;
---
+++ src/components/InvoiceGenerationPortal/InvoicePortal/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InvoicePortal = () => {
+  return (
+    <div>
+      <h1>Invoice Portal</h1>
+    </div>
+  );
+};
+
+export default InvoicePortal;
