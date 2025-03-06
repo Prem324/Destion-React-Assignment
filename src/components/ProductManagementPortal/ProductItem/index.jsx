e738e9452dd8afe6657aa54dec4bcfb6e224b156
Prem324 <prem324r@gmail.com>
2025-03-07 00:21:48 +0530

    Add api's to Products

--- src/components/ProductManagementPortal/ProductItem/index.jsx
+++ src/components/ProductManagementPortal/ProductItem/index.jsx
@@ -6,7 +6,7 @@ function ProductItem({ product }) {
   return (
     <div className="product-item">
       <Link className="product-details-link" to={`product/${product.id}`}>
-        <img className="product-image" src={product.image} />
+        <img className="product-image" src={product.productImage} />
         <h2 className="product-name">{product.name}</h2>
       </Link>
     </div>
