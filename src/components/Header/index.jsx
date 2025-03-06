2c32a3c20af794688b6089bf349b638912787d5a
Prem324 <prem324r@gmail.com>
2025-03-06 12:30:17 +0530

    Add Links to the Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <nav>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/invioce">Invoice Portal</Link>
+      </li>
+      <li>
+        <Link to="/product">Product Portal</Link>
+      </li>
+    </nav>
   );
 };
 
