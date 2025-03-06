160cf422d8ce9e6031f31e2edf93f32c6c61ac93
Prem324 <prem324r@gmail.com>
2025-03-06 12:26:01 +0530

    Add componenets and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import InvoicePortal from "./components/InvoiceGenerationPortal/InvoicePortal";
 import ProductManagement from "./components/ProductManagementPortal/ProductManagement";
 
 function App() {
   return (
-    <div>
-      <Navbar />
+    <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/invoice" element={<InvoicePortal />} />
         <Route path="/product" element={<ProductManagement />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
