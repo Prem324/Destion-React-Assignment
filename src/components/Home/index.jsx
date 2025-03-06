979fb1fc2360d98a60b0c3a33f7126282182d08f
Prem324 <prem324r@gmail.com>
2025-03-06 14:05:43 +0530

    Add Styles to the Home component

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import homeImage from "../../assets/product-invoice-2.jpg";
+import "./index.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home-container">
+      <div className="hero-section">
+        <h1>Welcome to Our Application</h1>
+        <p>Manage your invoices and products with ease.</p>
+      </div>
+      <div>
+        <img className="home-page-image" src={homeImage} alt="home-image" />
+      </div>
+      <div>
+        <button>Get Started</button>
+      </div>
     </div>
   );
 };
