67524b94d1f6389eff914417ce8a038a4843da66
Prem324 <prem324r@gmail.com>
2025-03-07 17:47:04 +0530

    Add Back Naviagaiotn button

--- src/components/ProductManagementPortal/ProductDetails/index.jsx
+++ src/components/ProductManagementPortal/ProductDetails/index.jsx
@@ -1,20 +1,28 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { IoMdArrowBack } from "react-icons/io";
 import products from "../../../products-content";
 import "./index.css";
 
 function ProductDetails() {
   const { productName } = useParams();
   const product = products.find((a) => a.productName === productName);
+  const navigate = useNavigate();
+  const onClickBack = () => {
+    navigate("/products");
+  };
   return (
-    <div className="product-detail-container">
-      <img src={product.imageUrl} alt={product.productName} />
-      <div className="product-details">
-        <h1>{product.productName}</h1>
-        <p>{product.description}</p>
-        <p>Price: {product.price}/-</p>
+    <>
+      <IoMdArrowBack className="back-nav" onClick={onClickBack} />
+      <div className="product-detail-container">
+        <img src={product.imageUrl} alt={product.productName} />
+        <div className="product-details">
+          <h1>{product.productName}</h1>
+          <p>{product.description}</p>
+          <p>Price: {product.price}/-</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
