7af9166961e1768c86f93174e65fa34d9fb10448
Prem324 <prem324r@gmail.com>
2025-03-07 12:36:19 +0530

    Add some styling to Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./index.css";
+import homeIcon from "../../assets/home.png";
+import productIcon from "../../assets/product-2.png";
+import invoiceIcon from "../../assets/invoice-2.png";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const onClickHeaderName = () => {
+    navigate("/");
+  };
   return (
     <nav>
+      <h1 onClick={onClickHeaderName}>Invoices and Product Manager</h1>
       <ul>
         <li>
           <Link to="/" className="link-item">
-            Home
+            <img src={homeIcon} alt="home-icon" />
+            <p>Home</p>
           </Link>
         </li>
 
         <li>
           <Link to="/product" className="link-item">
-            Product
+            <img src={productIcon} alt="product-icon" />
+            <p>Product Management</p>
           </Link>
         </li>
         <li>
           <Link to="/invoice" className="link-item">
-            Invoice
+            <img src={invoiceIcon} alt="invoice-icon" />
+            <p>Invoice Generation</p>
           </Link>
         </li>
       </ul>
